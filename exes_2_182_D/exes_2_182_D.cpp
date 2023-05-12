b953580f233e21924a9ef4456312061bf427a078
luthfibphoenix <lutfibahtiar2409@gmail.com>
2023-05-12 11:23:47 +0700

    fix

--- exes_2_182_D/exes_2_182_D.cpp
+++ exes_2_182_D/exes_2_182_D.cpp
@@ -3,6 +3,10 @@ using namespace std;
 
 int luthfi(33);
 int n;
+int i;
+int upperbound = n - 1;
+int lowerbound = 0;
+int mid = (upperbound + lowerbound) / 2;
 
 void input()
 {
@@ -14,4 +18,37 @@ void input()
 			break;
 		else cout << "\nArray harus mempunyai minimal 1 dan maksimal 33 elements' \n\n";
 	}
+}
+
+void BinarySearch() 
+{
+	 int low = 0;
+	 int high = n - 1;
+	 int mid = (high - low) / 2; 
+
+	 while (luthfi[mid] = n)
+	 {
+		 cout << "ditemukan" << endl;
+		 break;
+	 }
+	 if (luthfi[mid] < n)
+	 {
+		 upperbound = n - 1;
+	 }
+	 if (luthfi[mid] > n)
+	 {
+		 lowerbound = 0;
+	 }
+	 if (lowerbound + upperbound) / 2;
+	 {
+		 mid = (upperbound + lowerbound) / 2;
+	 }
+	 cout << "tidak ditemukan" << endl;
+}
+
+int main()
+{
+	void input();
+	void BinarySearch();
+	return 0;
 }
